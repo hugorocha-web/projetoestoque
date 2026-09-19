48d27d8284a2b2b1437925338910adef464cf650
hugorocha-web <hjj78092@gmail.com>
2026-09-19 18:08:42 -0300

    comecei o front

--- back-end/index.js
+++ back-end/index.js
@@ -1,32 +1,55 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express()
 
 
+//primeiro iniciar o mongoose
+process.loadEnvFile()
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=> console.log('banco conectado com sucesso'))
+//agora conectei o bando e adicionei o then 
+.catch((error)=> console.log('erro ao conectar o banco', error))
 
-let produtos = [
-    {
-        "nome":"caderno", 
-        "preço": 23,
-        "estoque": 123
-    }
-]
+//isso cria um formato de arquivo pro banco
+const produtoSchema = new mongoose.Schema({
+    nome: {type: String, required: true},
+    preco: {type: Number, required: true},
+    estoque: {type: Number, required: true},
+    categoria: {type: String, required: true},
+    descrição: {type: String, required: true},
+    imagem: {type: String, required: true}
+}, {timestamps: true}
+)
 
+//criei o produto agora é mandar pra ele modelar
+const Produto = mongoose.model('Produto', produtoSchema)
+
+
+
+app.use(cors())
 app.use(express.json())
 
 
 app.get('/produtos',  async (req, res) =>{
-    res.send(produtos)
+    let produtos = await Produto.find()
+    req.json(produtos)
 
 })
 
+app.put('/produtos:id', async (req, res) => {
+    
+
+
+
+})
 
 app.post('/produtos', async (req, res) => {
 
 
     let produto = req.body
     res.json(produto)
-    produtos.push(produto)
 
 
 })
